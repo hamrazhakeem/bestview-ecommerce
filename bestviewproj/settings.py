b1273487c4801d7b3a9a6a7ce9df1f0c6f917214
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-04-03 10:49:14 +0530

    refactor: modify allowed host

--- bestviewproj/settings.py
+++ bestviewproj/settings.py
@@ -31,12 +31,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", 'localhost', 'bestview-ecommerce-production.up.railway.app']
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", 'localhost', 'bestview-ecommerce-production-a076.up.railway.app']
 
 SITE_ID = 7
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://bestview-ecommerce-production.up.railway.app",
+    "bestview-ecommerce-production-a076.up.railway.app",
 ]
 
 # Application definition
