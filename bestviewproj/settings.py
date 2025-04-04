363ea03003e6a83aa2d524cd49cdf6d222551942
Hamraz Hakeem <hamrazhakeem100@gmail.com>
2025-04-04 16:31:14 +0530

    feat: configure s3 for media files

--- bestviewproj/settings.py
+++ bestviewproj/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     "user_order_and_payment",
     "admin_order_and_payment",
     "paypal.standard.ipn",
+    "storages",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
@@ -177,9 +178,17 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
-MEDIA_URL = "/media/"
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+AWS_DEFAULT_ACL = None
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# Storage and Media Configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
